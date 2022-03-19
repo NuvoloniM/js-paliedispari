24639d32b0edaa7520dff9da3a5429c4b73c38dc
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-19 17:05:31 +0100

    Bonus.js

--- es2/bonus.js
+++ es2/bonus.js
@@ -1,69 +1,30 @@
-/*
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-*/
+//cosa deve fare il programma:
+
+//seleziono pari o dispari
+//scelgo un numero tra 1 e 5
+//schiaccio play -> inizia il gioco 
+
+//la funzione start deve registrare le scelte, sommare i numeri e dare il risultato
+
+//variabili 
+let compNumber = 0;
+let userNumb = 0;
+
 //target i btn in html
 let btn1 = document.getElementById('btn_1');
 let btn2 = document.getElementById('btn_2');
 let btn3 = document.getElementById('btn_3');
 let btn4 = document.getElementById('btn_4');
+//target btn inizio gioco 
 let btnGo = document.getElementById('btn_go');
 
-//chiamo le funzioni con gli eventi
+//1. Seleziono Pari o Dispari
 btn1.addEventListener("click", btnEven);
 btn2.addEventListener("click",btnOdd);
+
+//2.Schiaccio play -> inizo il gioco 
+//funzione start game
 btnGo.addEventListener("click",check);
-
-
-
-
-let userChoice = 0;
-if (btn1.classList.contains('btn-success') == true) {
-    userChoice = 'Pari';
-} else {
-    userChoice = 'Dispari';
-}
-console.log(userChoice);
-
-// let userNumb = document.querySelector('input').value;
-console.log(userNumb)
-
-//funzione per scegliere un numero tra min e max
-let max = 5;
-let x = 0;
-
-function randomInt(max) {
-    x = Math.ceil(Math.random()*max);
-    console.log(x);
-    return x;
-}
-//estraggo il numero del computer usando la funzione con max=5;
-let compNumber= randomInt(5);
-
-//sommo i due numeri 
-let sum = userNumb + compNumber ;
-console.log(sum);
-
-//funzione generica per capire se il parametro è pari o dispari 
-function isEven(x) {
-    if (x % 2 == 0) {
-        return 'Pari';
-    } else {
-        return 'Dispari';
-    }
-}
-console.log(isEven(sum));
-//Chi vince?
-if (userChoice == isEven(sum)) {
-    console.log('Hai vinto!!');
-} else {
-    console.log('Hai perso!');
-}
-
 
 
 //funzioni
@@ -82,20 +43,65 @@ function btnOdd() {
     btn4.classList.replace("btn-danger", "btn-outline-danger");
 }
 
-//controll se il numero inserito va bene e controlla il btn scelto.
-function check() {
-    let val = document.querySelector('input').value;
-    if (isNaN(val) == true || val < 1 || val > 5 ) {
-       alert("Numero non valido");
-    } else {
-        userNumb = document.querySelector('input').value;
-    }
+//registrare scelte, sommare numeri e dare risultato
 
-    if (btn1.classList.contains('btn-success') == true) {
-    userChoice = 'Pari';
+function check() {
+    //controllo se i dati sono tutti inseriti
+    if (btn1.classList.contains('btn-success') && btn2.classList.contains('btn-outline-danger') || btn1.classList.contains('btn-outline-success') && btn2.classList.contains('btn-danger')) {
+        //controllo user's number input 
+        let val = document.querySelector('input').value;
+        if (isNaN(val) == true || val < 1 || val > 5 ) {
+        alert("Numero non valido");
+        } else {
+            userNumb = parseInt(val);
+            document.getElementById('user_numb').innerHTML = `${userNumb}`;
+        }
+
+        //controllo user choice
+        if (btn1.classList.contains('btn-success') == true) {
+        userChoice = 'Pari';
+        } else {
+        userChoice = 'Dispari';
+        }
+        console.log(userChoice);
+        
+        //estraggo comp number
+        let compNumber= randomInt(5);
+        document.getElementById('comp_numb').innerHTML = `${compNumber}`;
+
+        //sommo i due numeri 
+        let sum = userNumb + compNumber;
+        document.getElementById('sum').innerHTML = `${sum}`;
+        //Chi vince?
+        if (userChoice == isEven(sum)) {
+            document.getElementById('box_result').classList.add('bg_green'); 
+            document.getElementById('result').innerHTML = `${isEven(sum)} : HAI VINTO!!!`;
+        } else {
+            document.getElementById('box_result').classList.add('bg_red'); 
+            document.getElementById('result').innerHTML = `${isEven(sum)} : HAI PERSO!!!`;
+        }
     } else {
-    userChoice = 'Dispari';
+        alert('Inserisci tutti i dati');
     }
-    console.log(userChoice);
     
-  }
+    
+}
+
+
+//funzione per scegliere un numero tra min e max
+
+let x = 0;
+function randomInt(max) {
+    x = Math.ceil(Math.random()*max);
+    console.log(x);
+    return x;
+}
+
+//funzione generica per capire se il parametro è pari o dispari 
+function isEven(x) {
+    if (x % 2 == 0) {
+        return 'Pari';
+    } else {
+        return 'Dispari';
+    }
+}
